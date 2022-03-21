2a4e39673d8b2ccc577e7cb1291b3160f99ba253
Alex <alejandrogamino@protonmail.com>
2022-03-21 23:55:53 +0100

    More tests and fixes for checkRollStr

--- Mythras/Characters/src/test/roll_test.cc
+++ Mythras/Characters/src/test/roll_test.cc
@@ -3,7 +3,6 @@
 
 // Check invalid characters in roll strings
 TEST(RollTest, CheckInvalidCharsRollStr) {
-  Roll r("prueba");
   EXPECT_FALSE(Roll::goodRollStr("A"));
   EXPECT_FALSE(Roll::goodRollStr("1A"));
   EXPECT_FALSE(Roll::goodRollStr("1D8"));
@@ -25,4 +24,82 @@ TEST(RollTest, CheckFixedRollStr) {
   EXPECT_FALSE(Roll::goodRollStr("01"));
   EXPECT_FALSE(Roll::goodRollStr("01d3"));
 
+}
+
+
+// Check simple dice roll strings
+TEST(RollTest, CheckSimpleRollStrings) {
+  EXPECT_FALSE(Roll::goodRollStr("3d"));
+
+  EXPECT_FALSE(Roll::goodRollStr("0d2"));
+  EXPECT_TRUE(Roll::goodRollStr("1d2"));
+  EXPECT_TRUE(Roll::goodRollStr("2d2"));
+  EXPECT_TRUE(Roll::goodRollStr("4d2"));
+  EXPECT_TRUE(Roll::goodRollStr("10d2"));
+  EXPECT_TRUE(Roll::goodRollStr("16523d2"));
+
+  EXPECT_FALSE(Roll::goodRollStr("0d3"));
+  EXPECT_TRUE(Roll::goodRollStr("1d3"));
+  EXPECT_TRUE(Roll::goodRollStr("2d3"));
+  EXPECT_TRUE(Roll::goodRollStr("4d3"));
+  EXPECT_TRUE(Roll::goodRollStr("10d3"));
+  EXPECT_TRUE(Roll::goodRollStr("9846d3"));
+
+  EXPECT_FALSE(Roll::goodRollStr("0d4"));
+  EXPECT_TRUE(Roll::goodRollStr("1d4"));
+  EXPECT_TRUE(Roll::goodRollStr("2d4"));
+  EXPECT_TRUE(Roll::goodRollStr("4d4"));
+  EXPECT_TRUE(Roll::goodRollStr("10d4"));
+  EXPECT_TRUE(Roll::goodRollStr("94651d4"));
+
+  EXPECT_FALSE(Roll::goodRollStr("0d6"));
+  EXPECT_TRUE(Roll::goodRollStr("1d6"));
+  EXPECT_TRUE(Roll::goodRollStr("2d6"));
+  EXPECT_TRUE(Roll::goodRollStr("4d6"));
+  EXPECT_TRUE(Roll::goodRollStr("10d6"));
+  EXPECT_TRUE(Roll::goodRollStr("164646d6"));
+
+  EXPECT_FALSE(Roll::goodRollStr("0d8"));
+  EXPECT_TRUE(Roll::goodRollStr("1d8"));
+  EXPECT_TRUE(Roll::goodRollStr("2d8"));
+  EXPECT_TRUE(Roll::goodRollStr("4d8"));
+  EXPECT_TRUE(Roll::goodRollStr("10d8"));
+  EXPECT_TRUE(Roll::goodRollStr("46842d8"));
+
+  EXPECT_FALSE(Roll::goodRollStr("0d10"));
+  EXPECT_TRUE(Roll::goodRollStr("1d10"));
+  EXPECT_TRUE(Roll::goodRollStr("2d10"));
+  EXPECT_TRUE(Roll::goodRollStr("4d10"));
+  EXPECT_TRUE(Roll::goodRollStr("10d10"));
+  EXPECT_TRUE(Roll::goodRollStr("5683012d10"));
+
+  EXPECT_FALSE(Roll::goodRollStr("0d12"));
+  EXPECT_TRUE(Roll::goodRollStr("1d12"));
+  EXPECT_TRUE(Roll::goodRollStr("2d12"));
+  EXPECT_TRUE(Roll::goodRollStr("4d12"));
+  EXPECT_TRUE(Roll::goodRollStr("10d12"));
+  EXPECT_TRUE(Roll::goodRollStr("68416105d12"));
+
+  EXPECT_FALSE(Roll::goodRollStr("0d20"));
+  EXPECT_TRUE(Roll::goodRollStr("1d20"));
+  EXPECT_TRUE(Roll::goodRollStr("2d20"));
+  EXPECT_TRUE(Roll::goodRollStr("4d20"));
+  EXPECT_TRUE(Roll::goodRollStr("10d20"));
+  EXPECT_TRUE(Roll::goodRollStr("189165088d20"));
+
+  EXPECT_FALSE(Roll::goodRollStr("1d20d12"));
+
+}
+
+
+// Check dice roll strings with addition or substraction
+TEST(RollTest, CheckAddSubstRollStrings) {
+  EXPECT_TRUE(Roll::goodRollStr("2d6+6"));
+  EXPECT_TRUE(Roll::goodRollStr("1+6"));
+  EXPECT_TRUE(Roll::goodRollStr("1d8-2+3"));
+  EXPECT_TRUE(Roll::goodRollStr("1d8+2d6"));
+  EXPECT_TRUE(Roll::goodRollStr("1d8+2d6-2+1d4"));
+  EXPECT_FALSE(Roll::goodRollStr("1d20+1d12d4"));
+  EXPECT_FALSE(Roll::goodRollStr("7+1d8+1-3d"));
+  EXPECT_TRUE(Roll::goodRollStr("7+1d8+1-3d2"));
 }
